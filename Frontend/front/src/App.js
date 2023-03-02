c57d1f8f3a7c30537122001dac2b2edc9ea74ced
Clécio de Lira Alves <cclecioc.com@gmail.com>
2023-03-02 16:08:38 -0300

    update

--- Frontend/front/src/App.js
+++ Frontend/front/src/App.js
@@ -1,16 +1,27 @@
 import './App.css';
 import Formulario from './Formulario';
 import Tabela from './Tabela';
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import Footer from './footer';
 
 function App() {
 
+  //UseState
   const [btnCadastrar, setBtnCadastrar] = useState(1)
+  const [produtos, setProdutos] = useState([])
 
+  //UseEffect
+  useEffect(()=> {
+    fetch('http://localhost:3000/produtos')
+    .then(retorno => retorno.json())
+    .then(retorno_convertido => setProdutos(retorno_convertido))
+  }, [])
+
+  //Retorno
   return (
     <div>
       <h1>Product Stock</h1>
+      <p>{JSON.stringify(produtos)}</p>
       <Formulario botao={btnCadastrar} />
       <Tabela />
       <Footer />
