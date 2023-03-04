5d0e107be6d10e7a840919d29a3f852b62a8092d
Clécio de Lira Alves <cclecioc.com@gmail.com>
2023-03-03 23:16:26 -0300

    update

--- Frontend/front/src/Formulario.js
+++ Frontend/front/src/Formulario.js
@@ -1,15 +1,15 @@
-function Formulario({botao}) {
+function Formulario({botao, eventoTeclado, cadastrar}) {
     return (
         <form>
             <label for="inome"><strong>Nome</strong></label>
-            <input type="text" name="nome" id="inome" placeholder="Nome do produto" autocomplete="off" className="form-control"/>
+            <input type="text" onChange={eventoTeclado} name="nome" id="inome" placeholder="Nome do produto" autocomplete="off" className="form-control"/>
             <label for="imarca"><strong>Marca</strong></label>
-            <input type="text" name="marca" id="imarca" placeholder="Marca do produto" autocomplete="off" className="form-control"/>
+            <input type="text" onChange={eventoTeclado} name="marca" id="imarca" placeholder="Marca do produto" autocomplete="off" className="form-control"/>
 
             {
                 botao
                 ?
-                <input type="button" value="Cadastrar" className="btn btn-primary"></input>
+                <input type="button" value="Cadastrar" onClick={cadastrar} className="btn btn-primary"></input>
                 :
                 <div>
                     <input type="button" value="Alterar"   className="btn btn-warning"></input>
