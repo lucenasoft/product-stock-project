57f7cdf83bd617c08cbdb25d15b684c38c780cf8
Clécio de Lira Alves <cclecioc.com@gmail.com>
2023-03-04 21:40:37 -0300

    update

--- Frontend/front/src/App.js
+++ Frontend/front/src/App.js
@@ -41,7 +41,14 @@ function App() {
     })
     .then(retorno => retorno.json())
     .then(retorno_convertido => {
-      console.log(retorno_convertido);
+
+      if (retorno_convertido.mensagem !== undefined) {
+        alert(retorno_convertido.mensagem);
+      } else {
+        setProdutos([...produtos, retorno_convertido]);
+        alert("Produto cadastrado com sucesso!");
+      }
+      
     })
   }
 
